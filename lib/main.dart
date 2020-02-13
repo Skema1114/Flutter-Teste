a181305bfb91269a279540391b39285344786e4b
Rafael Aranda Martins <skema1114@hotmail.com>
2020-02-13 17:27:25 -0300

    Começando curso 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutterteste/telas/transferencia/lista.dart';
 
-void main() => runApp(TesteApp());
+void main() => runApp(TesteAppPersistencia());
 
 // WIDGET RELACIONADO APENAS AO APP, ABSTRAÇÃO AO MAXIMO
-class TesteApp extends StatelessWidget {
+class TesteAppPersistencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.green[900],
-        accentColor: Colors.blueAccent[700],
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.blueAccent[700],
-          textTheme: ButtonTextTheme.primary,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
         ),
       ),
-      home: ListaTransferencias(),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutterteste/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(TesteApp());
+    await tester.pumpWidget(TesteAppPersistencia());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
